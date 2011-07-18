8d3bd12ff796c71616f813257c994fb7b358b27c
Adam Carlile <adam@benchmedia.co.uk>
2011-07-18 11:55:36 +0100

    Added latest updates

--- config/initializers/site_settings.rb
+++ config/initializers/site_settings.rb
@@ -1,11 +1,20 @@
 SITE_NAME = 'Paul Carlile, Restorative Principles in Practice'
-ADMIN_EMAIL = 'adam@benchmedia.com'
+ADMIN_EMAIL = 'paul@paulcarlile.com'
+DEV_EMAIL = 'adam@benchmedia.com'
 EMAIL_FROM = 'Paul Carile Education <website@paulcarlile.com>'
 
 TWITTER_USER = 'paulcarlile'
-TWITTER_PASS = '12dozen'
+TWITTER_PASS = ''
 
 RECAPTCHA_PUBLIC_KEY = '6LfBRwoAAAAAAJJFmTPWaywUVP3aD_pjpM3bOQrM'
 RECAPTCHA_PRIVATE_KEY = '6LfBRwoAAAAAADFrHBlQkd34eIrrzUt29XNTTpDk'
 ENV['RECAPTCHA_PUBLIC_KEY'] = '6LfBRwoAAAAAAJJFmTPWaywUVP3aD_pjpM3bOQrM'
 ENV['RECAPTCHA_PRIVATE_KEY'] = '6LfBRwoAAAAAADFrHBlQkd34eIrrzUt29XNTTpDk'
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+    :address => "smtp.sendgrid.net",
+    :domain => "paulcarlile.com",
+    :user_name => "hello@adamcarlile.com",
+    :password => ""
+}
